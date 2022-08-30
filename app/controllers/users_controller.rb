2ca15a5c4f0e5ccd23f46a747cf2a9deb6f7ad4e
Luis Enrique Guzman Pineda <luis.guzp13@gmail.com>
2022-08-30 17:58:30 -0500

    add user profile card

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
   def show
-    @user = User.find(params[:id])
-  end
-
-  def create
-    @user = User.new(params[user_params])
-    @user.save
-    redirect_to root_path
-  end
-
-  def update
-    @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to root_path
   end
 
   def destroy
@@ -23,7 +11,7 @@ class UsersController < ApplicationController
 
   private
 
-  def user_params
-    params.require(:user).permit(:first_name, :last_name)
+  def set_user
+    @user = current_user
   end
 end
