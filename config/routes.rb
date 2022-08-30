7d8179bb1a6706aaaf5939aab0a5bd1e151996ce
Avelina Maier <maieravelina@gmail.com>
2022-08-30 15:47:44 -0500

    deleted some spaces in route

--- config/routes.rb
+++ config/routes.rb
@@ -17,10 +17,6 @@ Rails.application.routes.draw do
       end
     end
   end
-
-
-
-
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
