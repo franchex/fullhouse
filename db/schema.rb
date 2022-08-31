3e35851feb5cd9da838681c1adfde3ffc1a6c362
Francisco Sotelo <franciscosotelov@gmail.com>
2022-08-31 12:59:51 -0500

    partial and fontawesome

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_29_221644) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_30_224609) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_29_221644) do
     t.bigint "space_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "amount"
     t.index ["space_id"], name: "index_bills_on_space_id"
     t.index ["user_id"], name: "index_bills_on_user_id"
   end
