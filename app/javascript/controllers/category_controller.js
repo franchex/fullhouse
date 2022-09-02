628225d948132bb40d4807c4b99ef2e2a3ebb3e2
Francisco Sotelo <franciscosotelov@gmail.com>
2022-09-02 12:47:07 -0500

    partial bill chores active and inactiv

--- app/javascript/controllers/category_controller.js
+++ app/javascript/controllers/category_controller.js
@@ -2,16 +2,21 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="category"
 export default class extends Controller {
-  next(event) {
-
+  connect() {
+    console.log(this.element)
   }
 
   clickedCategory(event){
     let category = event.currentTarget.innerText
     let partial = document.querySelector(`.${category}`)
+    let active_partial= document.querySelector(".active")
+    active_partial.classList.add("inactive")
+    active_partial.classList.remove("active")
     partial.classList.remove("inactive")
+    partial.classList.add("active")
     console.log(partial)
     console.log(category)
+
   }
 
 
