755784367078665ea238ff908c893811f96e97ec
Avelina Maier <maieravelina@gmail.com>
2022-09-05 17:57:09 -0500

    added a lot of shit yall already know

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,5 +1,5 @@
 class Bill < ApplicationRecord
   belongs_to :payer, class_name: "User", foreign_key: "user_id"
   belongs_to :space
-  validates :amount, :name, :due_date, presence: true
+  validates :amount, :due_date, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_05_151641) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_05_223403) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,7 +24,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_05_151641) do
   end
 
   create_table "bills", force: :cascade do |t|
-    t.string "name"
     t.date "due_date"
     t.bigint "user_id", null: false
     t.bigint "space_id", null: false
